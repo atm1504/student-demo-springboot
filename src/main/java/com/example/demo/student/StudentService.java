4eff45c7e3acd15aa56bfa41906191526af58fa6
Amartya Mondal <atm1504.in@gmail.com>
2021-07-05 19:14:06 +0530

    Configure postgres

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,10 +1,13 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudents() {
         List<Student> arr = new ArrayList<Student>();
